32f6e2bcdde06bad1a1e15f07416d34da36f9024
murmelisotilas <105284237+murmelisotilas@users.noreply.github.com>
2022-06-07 12:44:06 +0300

    part5 a

---
+++ part5/bloglist-frontend/src/services/logins.js
@@ -0,0 +1,12 @@
+import axios from 'axios'
+const baseUrl = '/api/login'
+
+const login = async credentials => {
+  const response = await axios.post(baseUrl, credentials)
+  console.log('this is the response data from ',response.data)
+  return response.data
+}
+
+const loginServices = {login}
+
+export default loginServices
